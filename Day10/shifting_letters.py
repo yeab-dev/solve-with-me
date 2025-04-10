824e45a036192c5966faaac20ba39f8a88a7cbc5
yeabsera <yeab.dev@gmail.com>
2025-04-10 15:36:24 +0300

    feat: add solution for problem #848

--- Day10/shifting_letters.py
+++ Day10/shifting_letters.py
@@ -1,28 +1,13 @@
 from typing import List
-
 class Solution:
-    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        n = len(s)
-        prefix_diff = [0] * (n + 1)
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        strList = list(s)
+        for i in range(len(s)-2, -1, -1):
+            shifts[i] += shifts[i+1]
         
-        for start, end, direction in shifts:
-            if direction == 1:
-                prefix_diff[start] += 1
-                if end + 1 < n + 1:
-                    prefix_diff[end + 1] -= 1
-            else:
-                prefix_diff[start] -= 1
-                if end + 1 < n + 1:
-                    prefix_diff[end + 1] += 1
-        
-        res = []
-        current_shift = 0
-        for i in range(n):
-            current_shift += prefix_diff[i]
-            original_char = s[i]
-            new_order = ord(original_char) - ord('a') + current_shift
-            new_order %= 26
-            new_char = chr(ord('a') + new_order)
-            res.append(new_char)
-        
-        return ''.join(res)
+        for i, shift in zip(range(len(s)), shifts):
+            originalChar = strList[i]
+            newOrder = ord(originalChar) - ord('a') + shift
+            newOrder %= 26
+            strList[i] = chr(newOrder + ord('a'))
+        return "".join(strList)

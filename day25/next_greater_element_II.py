b21fd1dac4e8cea5bd6b2e15ec3acba4e88e6229
yeabsera <yeab.dev@gmail.com>
2025-05-08 10:02:44 +0300

    feat: add solution for problem #503

---
+++ day25/next_greater_element_II.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        stack = []
+        res = [-1] * len(nums)
+        for i in range(len(nums)):
+            if stack and nums[stack[-1]] < nums[i]:
+                while stack and nums[stack[-1]] < nums[i]:
+                    res[stack.pop()] = nums[i]
+            stack.append(i)
+        
+        if stack:
+            i = 0
+            while i < len(nums):
+                if nums[stack[-1]] < nums[i]:
+                    res[stack.pop()] = nums[i]
+                else:
+                    i += 1
+        return res

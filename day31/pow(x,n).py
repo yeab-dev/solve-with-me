c7af269c3c6fc96d280ee5d08cc5e4cdb8ac86ee
yeabsera <yeab.dev@gmail.com>
2025-05-15 17:03:41 +0300

    feat: add Solution for problem 50. Pow(x, n)

---
+++ day31/pow(x,n).py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def fast_pow(x, n):
+            if n == 0:
+                return 1.0
+            half = fast_pow(x, n // 2)
+            if n % 2 == 0:
+                return half * half
+            else:
+                return half * half * x
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+        return fast_pow(x, n)
+
+# My first code overflawed the stack.
+# After looking up some optimization
+# methods I came up with this

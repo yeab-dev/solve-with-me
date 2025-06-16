49becacb1694cfc8e60b48879bf2aeab0adc38dd
yeabsera <yeab.dev@gmail.com>
2025-06-16 11:07:48 +0300

    feat: add solution for problem 236

---
+++ day41/lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, x, left: 'TreeNode' = None, right: 'TreeNode' = None):
+        self.val = x
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root.val == p.val or root.val == q.val:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+
+        if left and right:
+            return root
+        return left if left else right

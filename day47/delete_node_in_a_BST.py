1406ba586289200b709ca86356099d268e6025f6
yeabsera <yeab.dev@gmail.com>
2025-07-08 16:01:02 +0300

    feat: add solution for problem #450

---
+++ day47/delete_node_in_a_BST.py
@@ -0,0 +1,31 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        def findSmallestNode(root):
+            curr = root
+            while curr and curr.left:
+                curr = curr.left
+            return curr
+        if not root:
+            return None
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                smallestNode = findSmallestNode(root.right)
+                root.val = smallestNode.val
+                root.right = self.deleteNode(root.right, smallestNode.val)
+        return root
